d6ac7689dcf6bb81a84c8c1f1440c564618b373f
Manasi2002 <70650481+Manasi2002@users.noreply.github.com>
2022-09-07 23:13:36 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ function Footer()
     return(
             <div className="container-fluid text-center bg-dark" style= {{marginBottom:0}} id="footer">       
                 <p className="text-light" style={{color: "white",fontSize: "110%"}}>Contact US:</p>     
-                <a href="/" className="font-weight-light text-light"style={{color: "white",fontSize: "110%"}}><i className="fas fa-phone" id="call-fafa" style={{color: "white",fontSize: "120%"}}></i> +91 7989391182  </a>
+                <a href="/" className="font-weight-light text-light"style={{color: "white",fontSize: "110%"}}><i className="fas fa-phone" id="call-fafa" style={{color: "white",fontSize: "120%"}}></i> +91 1234567899  </a>
                 <a href="/" className="font-weight-light text-light ml-3"style={{color: "white",fontSize: "110%"}}><i className="fas fa-envelope" id="mail-fafa" style={{color: "white",fontSize: "120%" }}>
-                </i> phemamanasi@gmail.com</a>
+                </i> abc@gmail.com</a>
             </div>
 
     );
